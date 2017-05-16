6e3617afe9e69f3266aac1f7ae82788a65201775
Jose Escalante <joe.escalante@gmail.com>
2017-05-16 17:43:02 -0300

    Adding carrierwave to gif

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,2 +1,11 @@
+require "carrierwave"
+
 class Gif < ApplicationRecord
+  validates :url, file_size: { less_than: 10.megabytes }
+
+  storage :aws
+
+  def size_range
+    1..10.megabytes
+  end
 end
