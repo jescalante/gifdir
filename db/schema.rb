82fa8397342f5cc636a61b9e31a788113c0f3d07
Jose Escalante <joe.escalante@gmail.com>
2017-05-16 17:35:02 -0300

    Updating db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170516191151) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "gifs", force: :cascade do |t|
     t.string "url"
     t.datetime "created_at", null: false
