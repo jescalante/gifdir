10198d06d6c162c51769e1f910811e0abc80a043
Jose Escalante <joe.escalante@gmail.com>
2017-05-16 18:10:06 -0300

    Setting uploading class in gif

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,11 +1,5 @@
-require "carrierwave"
-
 class Gif < ApplicationRecord
   validates :url, file_size: { less_than: 10.megabytes }
 
-  storage :aws
-
-  def size_range
-    1..10.megabytes
-  end
+  mount_uploader :file, GifUploader
 end
