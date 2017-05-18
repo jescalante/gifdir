a7cd33fb512bfe7247d7306b3f6ace2c73ad764f
Jose Escalante <joe.escalante@gmail.com>
2017-05-18 18:14:23 -0300

    Setting has many trhough in gif model

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,4 +1,7 @@
 class Gif < ApplicationRecord
+  has_many :gif_tags
+  has_many :tags, through: :gif_tags
+
   validates :file, file_size: { less_than: 10.megabytes }
 
   mount_uploader :file, GifUploader
