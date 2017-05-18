6ea41e717b34a42642743700f0dc4ea4f630daf8
Jose Escalante <joe.escalante@gmail.com>
2017-05-18 18:15:51 -0300

    Handling tags addded when submitting gif

--- app/controllers/gif_controller.rb
+++ app/controllers/gif_controller.rb
@@ -7,7 +7,15 @@ class GifController < ApplicationController
 
   def create
     @gif = Gif.new(gif_params)
+
     if @gif.save
+      tag_names = params[:tags].split(" ").reject(&:blank?)
+      tags = tag_names.map do |tag_name|
+        Tag.find_or_create_by(name: tag_name)
+      end
+
+      @gif.tags = tags
+
       flash[:notice] = "Gif uploaded successfully"
       redirect_to gif_new_path
     else
