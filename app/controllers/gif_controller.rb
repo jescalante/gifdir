be0538578fe350dd98e172d9e6379e3f0bb2fcf4
Jose Escalante <joe.escalante@gmail.com>
2017-05-18 16:25:03 -0300

    Handling gif post

--- app/controllers/gif_controller.rb
+++ app/controllers/gif_controller.rb
@@ -4,6 +4,18 @@ class GifController < ApplicationController
   end
 
   def create
-    @gif = Gif.new
+    @gif = Gif.new(gif_params)
+    if @gif.save
+      flash[:notice] = "Gif uploaded successfully"
+      redirect_to gif_new_path
+    else
+      flash[:alert] = @gif.errors.messages.values.first
+      render :new
+    end
+  end
+
+  private
+  def gif_params
+    params.require(:gif).permit(:file)
   end
 end
